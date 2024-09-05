c6b4f05b3ef9ad54d4b6dc67775e0e87f490ecbc
bepvte <8226605+bepvte@users.noreply.github.com>
2024-09-05 15:21:35 -0700

    fix(hideServers): rewrite to use stores

--- src/plugins/hideServers/index.tsx
+++ src/plugins/hideServers/index.tsx
@@ -17,12 +17,12 @@ import {
 } from "@api/ServerList";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
-import { GuildStore, Menu, React } from "@webpack/common";
+import { Menu, React, useStateFromStores } from "@webpack/common";
 import { Guild } from "discord-types/general";
 
-import HiddenServersButton from "./components/HiddenServersButton";
-import openHiddenServersModal from "./components/modal";
-import { addHidden, hiddenGuilds, hiddenGuildsDetail, loadHidden, settings } from "./settings";
+import hiddenServersButton from "./components/HiddenServersButton";
+import settings from "./settings";
+import { HiddenServersStore } from "./HiddenServersStore";
 
 type guildsNode = {
     type: "guild" | "folder";
@@ -38,14 +38,6 @@ type qsResult = {
     };
 };
 
-let rerenderRef: undefined | WeakRef<Function>;
-export function rerender() {
-    const ref = rerenderRef?.deref();
-    if (ref) {
-        ref();
-    }
-}
-
 const Patch: NavContextMenuPatchCallback = (
     children,
     { guild }: { guild: Guild; }
@@ -56,33 +48,22 @@ const Patch: NavContextMenuPatchCallback = (
         <Menu.MenuItem
             id="vc-hide-server"
             label="Hide Server"
-            action={() => addHidden(guild)}
+            action={() => HiddenServersStore.addHidden(guild)}
         />
     );
 };
 
-function serverListFunction() {
-    // if youve left a server dont show it in the count
-    const actuallyHidden = Array.from(hiddenGuilds).filter(x => GuildStore.getGuild(x)).length;
-    return <HiddenServersButton
-        count={actuallyHidden}
-        onClick={() => openHiddenServersModal({ servers: hiddenGuildsDetail() })}
-    ></HiddenServersButton>;
-}
-
-
 export function addIndicator() {
-    addServerListElement(ServerListRenderPosition.Below, serverListFunction);
+    addServerListElement(ServerListRenderPosition.Below, hiddenServersButton);
 }
 
 export function removeIndicator() {
-    removeServerListElement(ServerListRenderPosition.Below, serverListFunction);
+    removeServerListElement(ServerListRenderPosition.Below, hiddenServersButton);
 }
 
 export default definePlugin({
     name: "HideServers",
-    description:
-        "Allows you to hide servers from the guild list and quick switcher by right clicking them",
+    description: "Allows you to hide servers from the guild list and quick switcher by right clicking them",
     authors: [Devs.bep],
     tags: ["guild", "server", "hide"],
 
@@ -96,56 +77,57 @@ export default definePlugin({
             find: '("guildsnav")',
             replacement: [
                 {
-                    match: /(?<=Messages\.SERVERS,children:)(.+?\i)(\.map\(\i\))/,
-                    replace: "$1.flatMap($self.filterGuilds)$2",
+                    match: /(?<=Messages\.SERVERS,children:.{0,300}?)(\i)(\)?\.map\(\i\))/,
+                    replace: "$self.useFilteredGuilds($1)$2",
                 },
+                // despite my best efforts, the above doesnt trigger a rerender
                 {
                     match: /let{disableAppDownload.{0,10}isPlatformEmbedded/,
-                    replace:
-                        "$self.grabRerender(Vencord.Webpack.Common.React.useReducer(x => x+1,0));$&",
-                },
-            ],
+                    replace: "$self.useStore();$&",
+                }
+            ]
         },
         {
             find: "QUICKSWITCHER_PROTIP.format",
             replacement: {
                 match: /(?<=renderResults\(\){)let{query/,
-                replace:
-                    "this.props.results = $self.filterResults(this.props.results);$&",
+                replace: "this.props.results = $self.flteredGuildResults(this.props.results);$&",
             },
         },
     ],
     settings,
+    useStore: () => { useStateFromStores([HiddenServersStore], () => HiddenServersStore.hiddenGuilds, undefined, (old, newer) => old.size === newer.size); },
 
     async start() {
-        if (settings.store.showManager) {
+        if (settings.store.showIndicator) {
             addIndicator();
         }
-        await loadHidden();
+        await HiddenServersStore.load();
     },
 
     async stop() {
         removeIndicator();
-        hiddenGuilds.clear();
+        HiddenServersStore.unload();
     },
 
-    filterGuilds(guild: guildsNode): guildsNode[] {
-        if (hiddenGuilds.has(guild.id.toString())) {
-            return [];
-        }
-        const newGuild = Object.assign({}, guild);
-        newGuild.children = guild.children.filter(
-            child => !hiddenGuilds.has(child.id.toString())
-        );
+    useFilteredGuilds(guilds: guildsNode[]): guildsNode[] {
+        const hiddenGuilds = useStateFromStores([HiddenServersStore], () => HiddenServersStore.hiddenGuilds, undefined, (old, newer) => old.size === newer.size);
+        return guilds.flatMap((guild) => {
+            if (hiddenGuilds.has(guild.id.toString())) {
+                return [];
+            }
+            const newGuild = Object.assign({}, guild);
+            newGuild.children = guild.children.filter(
+                child => !hiddenGuilds.has(child.id.toString())
+            );
 
-        return [newGuild];
+            return [newGuild];
+        });
     },
 
-    grabRerender([, e]: [unknown, Function]) {
-        rerenderRef = new WeakRef(e);
-    },
-
-    filterResults(results: qsResult[]): qsResult[] {
+    filteredGuildResults(results: qsResult[]): qsResult[] {
+        // not used in a component so no useStateFromStore
+        const hiddenGuilds = HiddenServersStore.hiddenGuilds;
         return results.filter(result => {
             if (result?.record?.guild_id && hiddenGuilds.has(result.record.guild_id)) {
                 return false;
